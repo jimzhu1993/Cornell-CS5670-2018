c0fdf154a8f98b3fd5774309063381267d8e3b14
Zhu <zhu@umail.ucsb.edu>
2018-03-03 13:43:27 -0500

    finshied project 1

--- Project1_Hybrid_Images/hybrid.py
+++ Project1_Hybrid_Images/hybrid.py
@@ -3,6 +3,27 @@ sys.path.append('/Users/kb/bin/opencv-3.1.0/build/lib/')
 
 import cv2
 import numpy as np
+import math
+
+def apply_kernel(img, kernel, is_convolution):
+    img_row, img_col = img.shape
+    kernel_row, kernel_col = kernel.shape
+    # given kernel dimension is odd number
+    k_row_center = int(kernel_row / 2)
+    k_col_center = int(kernel_col / 2)
+    # augmenting the img to make computation easier
+    img_aug = np.pad(img, ((k_row_center, k_row_center), (k_col_center, k_col_center)), 'constant')
+    if not is_convolution:
+        kernel_filter = kernel.reshape((1, -1))[0]
+    else:
+        kernel_filter = kernel.reshape((1, -1))[0][::-1]
+
+    G = np.zeros((img_row, img_col), dtype=img.dtype)
+    for i in range(img_row):
+        for j in range(img_col):
+            feature_matrix = img_aug[i: i + kernel_row, j: j + kernel_col].reshape((1, -1))[0]
+            G[i, j] = feature_matrix.dot(kernel_filter)
+    return G
 
 def cross_correlation_2d(img, kernel):
     '''Given a kernel of arbitrary m x n dimensions, with both m and n being
@@ -23,7 +44,17 @@ def cross_correlation_2d(img, kernel):
         height and the number of color channels)
     '''
     # TODO-BLOCK-BEGIN
-    raise Exception("TODO in hybrid.py not implemented")
+
+    if kernel.shape[0] * kernel.shape[1] & 1 != 1:
+        raise Exception("kernel dimension is not odd")
+    else:
+        if len(img.shape) == 2:
+            return apply_kernel(img, kernel, False)
+        else:
+            G = np.zeros(img.shape, dtype = img.dtype)
+            for i in range(img.shape[2]):
+                G[:, :, i] = apply_kernel(img[:, :, i], kernel, False)
+    return G
     # TODO-BLOCK-END
 
 def convolve_2d(img, kernel):
@@ -40,7 +71,17 @@ def convolve_2d(img, kernel):
         height and the number of color channels)
     '''
     # TODO-BLOCK-BEGIN
-    raise Exception("TODO in hybrid.py not implemented")
+
+    if kernel.shape[0] * kernel.shape[1] & 1 != 1:
+        raise Exception("kernel dimension is not odd")
+    else:
+        if len(img.shape) == 2:
+            return apply_kernel(img, kernel, True)
+        else:
+            G = np.zeros(img.shape, dtype= img.dtype)
+            for i in range(img.shape[2]):
+                G[:, :, i] = apply_kernel(img[:, :, i], kernel, True)
+    return G
     # TODO-BLOCK-END
 
 def gaussian_blur_kernel_2d(sigma, width, height):
@@ -59,7 +100,25 @@ def gaussian_blur_kernel_2d(sigma, width, height):
         with an image results in a Gaussian-blurred image.
     '''
     # TODO-BLOCK-BEGIN
-    raise Exception("TODO in hybrid.py not implemented")
+
+    # rounds to the next odd integer
+    if width & 1 != 1:
+        width = width + 1
+    if height & 1 != 1:
+        height = height + 1
+
+    H = np.zeros((width, height))
+    k_row_center = int(width / 2)
+    k_col_center = int(height / 2)
+
+    for i in range(width):
+        for j in range(height):
+            x = (i - k_row_center)
+            y = (j - k_col_center)
+            H[i, j] = (1.0 / (2.0 * math.pi * sigma ** 2)) * (math.e ** (-(x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
+
+    return H / H.sum()
+
     # TODO-BLOCK-END
 
 def low_pass(img, sigma, size):
@@ -72,7 +131,10 @@ def low_pass(img, sigma, size):
         height and the number of color channels)
     '''
     # TODO-BLOCK-BEGIN
-    raise Exception("TODO in hybrid.py not implemented")
+
+    gaussian_kernel = gaussian_blur_kernel_2d(sigma, size, size)
+    return convolve_2d(img, gaussian_kernel)
+
     # TODO-BLOCK-END
 
 def high_pass(img, sigma, size):
@@ -85,7 +147,9 @@ def high_pass(img, sigma, size):
         height and the number of color channels)
     '''
     # TODO-BLOCK-BEGIN
-    raise Exception("TODO in hybrid.py not implemented")
+
+    return img - low_pass(img, sigma, size)
+
     # TODO-BLOCK-END
 
 def create_hybrid_image(img1, img2, sigma1, size1, high_low1, sigma2, size2,
@@ -113,5 +177,3 @@ def create_hybrid_image(img1, img2, sigma1, size1, high_low1, sigma2, size2,
     img2 *= 2 * mixin_ratio
     hybrid_img = (img1 + img2)
     return (hybrid_img * 255).clip(0, 255).astype(np.uint8)
-
-
